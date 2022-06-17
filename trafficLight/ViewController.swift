3c8812bb9457570eb93c6596d2efb2d120b46eac
Durov Dmitrii <durov-dmitrii@mail.ru>
2022-06-17 13:20:37 +0300

    Initial Commit

---
+++ trafficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trafficLight
+//
+//  Created by Дмитрий Дуров on 17.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
